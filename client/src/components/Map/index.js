f57d636033122f26a9245987e1b1f80a90f90341
Gal Elmalah <gale@wix.com>
2020-04-27 21:18:52 +0300

    added redirect from map to login

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,43 +1,62 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import * as style from './Map.module.scss';
 
-import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Map as LeafletMap, Marker, Popup, TileLayer } from 'react-leaflet';
 import { MarkersContext } from '../../providers/MapMarkersProvider/index';
+import { UserDetailsContext } from '../../providers/UserDetailsProvider';
+import { Redirect } from 'react-router-dom';
 
 export const Map = () => {
-  const {initialPosition, markers,draggableMarkerPosition,updateDraggableMarker,refmarker} = useContext(MarkersContext)
-    
-  return <div style={{height:'100vh'}}>
-  <LeafletMap className={style.map} center={draggableMarkerPosition || initialPosition} zoom={12}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    {markers.map(CustomMarker)}
-    {draggableMarkerPosition && 
-    <Marker 
-        className={style.dragMarker}
-          draggable={true}
-          onDragend={updateDraggableMarker}
-          position={draggableMarkerPosition}
-          ref={refmarker}>
-      <Popup>Draggable marker</Popup>
-    </Marker>
-    }
-  </LeafletMap>
-  </div>
-}
+  const {
+    initialPosition,
+    markers,
+    draggableMarkerPosition,
+    updateDraggableMarker,
+    refmarker,
+  } = useContext(MarkersContext);
+  const { userDetails } = useContext(UserDetailsContext);
+  if (!userDetails.name) {
+    return <Redirect to="/" />;
+  }
+  return (
+    <div style={{ height: '100vh' }}>
+      <LeafletMap
+        className={style.map}
+        center={draggableMarkerPosition || initialPosition}
+        zoom={12}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {markers.map(CustomMarker)}
+        {draggableMarkerPosition && (
+          <Marker
+            className={style.dragMarker}
+            draggable={true}
+            onDragend={updateDraggableMarker}
+            position={draggableMarkerPosition}
+            ref={refmarker}
+          >
+            <Popup>Draggable marker</Popup>
+          </Marker>
+        )}
+      </LeafletMap>
+    </div>
+  );
+};
 
-const CustomMarker =({location,name,description, type,id}) => <Marker key={id} position={location}>
-<Popup>
-  <span className={style.popoverContainer}>
-  <h4>Type:</h4>
-  <p>{type}</p>
-  <h4>Name:</h4>
-  <p>{name}</p>
-  <h4>Description:</h4>
-  <p>{description}</p>
-  </span>
-</Popup>
-
-</Marker>
+const CustomMarker = ({ location, name, description, type, id }) => (
+  <Marker key={id} position={location}>
+    <Popup>
+      <span className={style.popoverContainer}>
+        <h4>Type:</h4>
+        <p>{type}</p>
+        <h4>Name:</h4>
+        <p>{name}</p>
+        <h4>Description:</h4>
+        <p>{description}</p>
+      </span>
+    </Popup>
+  </Marker>
+);
