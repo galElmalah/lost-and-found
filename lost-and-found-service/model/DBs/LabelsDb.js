31afdc0ff908006ec10e08414d0830b48c86dcee
yoelz55 <yoelzeitoun08@gmail.com>
2020-06-12 10:15:23 +0300

    Add kind of labels

--- lost-and-found-service/model/DBs/LabelsDb.js
+++ lost-and-found-service/model/DBs/LabelsDb.js
@@ -1,8 +1,12 @@
 module.exports.LabelsDb = class LabelsDb {
   static getAll() {
     const labels = {
-      animales: ['dog', 'cat'],
-      miscs: ['keys', 'etc'],
+      animales: ['dog', 'cat', 'rabbit',],
+      miscs: ['keys', 'wallet', 'glasses', 'gloves', 'watch',],
+      electronic: ['phone', 'laptopt', 'electric bicycle', 'phone charger',
+        'headphone', ],
+      value: ['money', 'bank card',],
+      jewellery: ['earrings', 'ring', 'necklace', 'bracelet']
     };
     return Promise.resolve(labels);
   }
